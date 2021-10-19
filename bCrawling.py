9e91f62b9b7c5f59dce7c764c5497e19b268d4de
Ohjinn <hjin9445@gmail.com>
2021-10-19 10:19:18 +0900

    real30

--- bCrawling.py
+++ bCrawling.py
@@ -29,30 +29,30 @@ def getpic():
                 if extension != 'png' and 'jpg' and 'jpeg':
                     extension = 'jpg'
 
-                # application.urllib.request.urlretrieve(imgurl, "static/images/" + name + extension)
-                # application.db.userInfo.update_one({'name': name},
-                #                                    {'$set': {'pic': '../static/images/' + name + extension}})
-
-
-
-                tempimg = application.urllib.request.urlopen(imgurl).read()
-
-                s3 = boto3.client('s3',
-                                  aws_access_key_id=AWS_ACCESS_KEY_ID,
-                                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY
-                                  )
-                s3.put_object(
-                    ACL='public-read',
-                    Bucket=BUCKET_NAME,
-                    Body=tempimg,
-                    Key='images/' + name + '.' + extension,
-                    ContentType=tempimg.extention
-                )
-
+                application.urllib.request.urlretrieve(imgurl, "static/images/" + name + extension)
                 application.db.userInfo.update_one({'name': name},
-                                                   {'$set': {'pic': 'https://mysparta2.s3.ap-northeast-2.'
-                                                   'amazonaws.com/images/' + name + '.' + extension}})
+                                                   {'$set': {'pic': '../static/images/' + name + extension}})
 
+
+
+                # tempimg = application.urllib.request.urlopen(imgurl).read()
+                #
+                # s3 = boto3.client('s3',
+                #                   aws_access_key_id=AWS_ACCESS_KEY_ID,
+                #                   aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+                #                   )
+                # s3.put_object(
+                #     ACL='public-read',
+                #     Bucket=BUCKET_NAME,
+                #     Body=tempimg,
+                #     Key='images/' + name + '.' + extension,
+                #     ContentType=tempimg.extention
+                # )
+                #
+                # application.db.userInfo.update_one({'name': name},
+                #                                    {'$set': {'pic': 'https://mysparta2.s3.ap-northeast-2.'
+                #                                    'amazonaws.com/images/' + name + '.' + extension}})
+                #
 
 
             except urllib3.exceptions.LocationParseError:
